222b157239e6618f909d969eb72e2070771d1c45
Eugenio Depalo <eugeniodepalo@gmail.com>
2015-10-28 14:06:48 +0000

    Initialize popout application

--- addon/models/window.js
+++ addon/models/window.js
@@ -1,11 +1,38 @@
 import Ember from 'ember';
 
-export default Ember.Object.extend({
-  sendAction() {
+const { on, observer, run } = Ember;
 
+export default Ember.Object.extend({
+  reference: null,
+  application: null,
+  router: null,
+
+  initializeWindow: on('init', observer('reference', function() {
+    let reference = this.get('reference');
+
+    if (reference != null) {
+      reference.addEventListener('ember-popout:initialize', (event) => {
+        run(this, function() {
+          this.set('application', event.detail.application);
+        });
+      });
+    }
+  })),
+
+  initializeLookups: on('init', observer('application', function() {
+    let application = this.get('application');
+
+    if (application != null) {
+      this.set('router', application.container.lookup('router:main'));
+    }
+  })),
+
+  sendAction(...args) {
+    this.send(this.get('router.currentRouteName'), ...args);
   },
 
-  send(channel) {
-
+  send(channel, ...args) {
+    console.log(...args);
+    alert(channel);
   }
-})
+});
---
+++ app/instance-initializers/ember-poput.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+
+export function initialize(application) {
+  let event = new CustomEvent('ember-popout:initialize', {
+    detail: { application }
+  }, false);
+
+  window.dispatchEvent(event);
+}
+
+export default {
+  name: 'ember-popout',
+  initialize: initialize
+};
