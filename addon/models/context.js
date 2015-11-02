59f8b9c0ac01475e7594c13d3befaf20c675f9a1
Dmitri Akatov <akatov@gmail.com>
2015-11-02 23:55:08 +0000

    added some documentation

--- addon/models/context.js
+++ addon/models/context.js
@@ -4,15 +4,66 @@ import stringifyOptions from 'ember-popout/lib/stringify-options';
 
 const { run, merge, computed, RSVP } = Ember;
 
+/**
+ * @module models
+ */
+
+/**
+ * Represents the context of a popout window. This lives in the parent window.
+ * The `ember-popout` instance initializer of the popout window fires an event
+ * which an instance of `Context` listens to to initialize its properties to
+ * properly point to objects in the popout window. Calling `send` on a context
+ * triggers an event on the popout window's EmberPopout service. If a route
+ * in the popout window uses the EmberPopoutRoute mixin, it will translate
+ * such events into actions on the route.
+ *
+ * @class Context
+ */
 export default Ember.Object.extend({
+
   mergedProperties: ['options'],
 
+  /**
+   * A reference to the `window` object of an opened popout window.
+   * @property _reference
+   * @private
+   */
   _reference: null,
+
+  /**
+   * A reference to the application instance in the popout window.
+   * @property _application
+   * @private
+   */
   _application: null,
+
+  /**
+   * A reference to the router of the application instance in the popout window.
+   * @property _router
+   * @private
+   */
   _router: null,
+
+  /**
+   * A reference to the ember-popout service of the application instance in the
+   * popout window.
+   * @property _popout
+   * @private
+   */
   _popout: null,
 
+  /**
+   * @property isOpen
+   * @type Boolean
+   */
   isOpen: false,
+
+  /**
+   * The URL that will be used to open the popout window.
+   * @property url
+   * @type String
+   * @default window.location.origin the top most
+   */
   url: window.location.origin,
 
   id: computed(function() {
@@ -25,8 +76,8 @@ export default Ember.Object.extend({
   },
 
   _initializeLookups() {
+    // this is the container of the application instance in the poopout window
     let container = this.get('_application.container');
-
     this.set('_router', container.lookup('router:main'));
     this.set('_popout', container.lookup('service:ember-popout'));
   },
